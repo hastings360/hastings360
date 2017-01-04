e8493eb7040b71679559fdad17297de943795a27
Larry <hastings360@gmail.com>
2017-01-03 22:26:46 -0500

    1-3-17

---
+++ angular2Convert/smApp-qstart/app/app.module.js
@@ -0,0 +1,15 @@
+(function(app){
+    app.AppModule =
+
+        ng.core.NgModule({
+            imports: [ ng.platformBrowser.BrowserModule ],
+            declarations: [ app.AppComponent ],
+            bootstrap: [ app.AppComponent ]
+        })
+
+        .Class({
+            constructor: function(){}
+        })
+})
+
+(window.app || (window.app = {}));
