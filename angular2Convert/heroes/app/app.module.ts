e6f46639e7fff1e6d296c442550f7884733f7688
Hastings <hastingsl@GRD-8-75340.local>
2017-01-11 17:07:58 -0500

    1/11/17

--- angular2Convert/heroes/app/app.module.ts
+++ angular2Convert/heroes/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
+
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, ],
+  bootstrap:    [ AppComponent, ]
 })
 export class AppModule { }
