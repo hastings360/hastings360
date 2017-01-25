d93ea30f3927fc39a73350ddc0c794a9637fe919
Hastings <hastingsl@GRD-8-75340.local>
2017-01-25 13:36:24 -0500

    1/25/17

--- angular2Convert/heroes/app/app.module.ts
+++ angular2Convert/heroes/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -8,9 +9,22 @@ import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ],
+  imports:      [ BrowserModule, FormsModule, 
+  
+  RouterModule.forRoot([
+    {
+        path: 'heroes',
+        component: HeroesComponent
+    }
+])
+  
+  ],
   declarations: [ AppComponent, HeroDetailComponent, HeroesComponent],
   providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
+
+
+
