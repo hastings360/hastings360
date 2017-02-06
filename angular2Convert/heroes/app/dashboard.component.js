83e345929416e68100c087302988c86fc1b6826e
Hastings <hastingsl@GRD-8-75340.local>
2017-02-06 13:48:18 -0500

    2/6/17

--- angular2Convert/heroes/app/dashboard.component.js
+++ angular2Convert/heroes/app/dashboard.component.js
@@ -17,7 +17,7 @@ var DashboardComponent = (function () {
     }
     DashboardComponent.prototype.ngOnInit = function () {
         var _this = this;
-        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes.slice(1, 5); });
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
     };
     DashboardComponent = __decorate([
         core_1.Component({
