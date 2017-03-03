c826cecf85eca7436145420c8e31a4a87e5fef46
Hastings <hastingsl@GRD-8-75340.local>
2017-03-03 08:37:49 -0500

    3/3/17

--- angular2Convert/practice-angular-cli/src/app/user-list/user-list.component.ts
+++ angular2Convert/practice-angular-cli/src/app/user-list/user-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
   names: string[];
+  
   constructor() { 
     this.names = ['Jude', 'Wuan', 'Max', 'Mary'];
   }
