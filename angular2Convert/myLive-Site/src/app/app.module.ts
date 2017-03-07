9f003aa0b8112326ff13ca05e984e7c3cb4cb9b3
Hastings <hastingsl@GRD-8-75340.local>
2017-03-07 10:35:57 -0500

    3/7/17

--- angular2Convert/myLive-Site/src/app/app.module.ts
+++ angular2Convert/myLive-Site/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
