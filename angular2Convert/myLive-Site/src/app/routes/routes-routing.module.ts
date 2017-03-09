26bc9b1d44c3c1412b42adca0a73892c9fdcf5e9
Larry Hastings <larryhastings@lm.local>
2017-03-08 21:56:17 -0500

    3/8/17

--- angular2Convert/myLive-Site/src/app/routes/routes-routing.module.ts
+++ angular2Convert/myLive-Site/src/app/routes/routes-routing.module.ts
@@ -1,12 +1,14 @@
 
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '../home/home.component';
+import { ContactComponent } from '../contact/contact.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'bar', redirectTo: '/home', pathMatch: 'full'}
+  {path: '/', component: HomeComponent, data: {title: 'Larry Hastings'}},
+  {path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
