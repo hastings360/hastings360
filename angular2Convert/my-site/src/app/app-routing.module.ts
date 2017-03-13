7ddb85ab278662825a16e6e2d77a3633ec9cd2a2
Hastings <hastingsl@GRD-8-75340.local>
2017-03-13 15:34:56 -0400

    3/13/17

---
+++ angular2Convert/my-site/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, ViewEncapsulation } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent, data: {title: 'Larry Hastings'}},
+  { path: 'contact', component: ContactComponent, data: {title: 'Contact Info'}}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
---
+++ angular2Convert/my-site/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
