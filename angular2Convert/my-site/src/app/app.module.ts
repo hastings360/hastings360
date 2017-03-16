af6980356e986be5549390a1d008a078d9854542
Hastings <hastingsl@GRD-8-75340.local>
2017-03-16 09:36:59 -0400

    3/16/17

--- angular2Convert/my-site/src/app/app.module.ts
+++ angular2Convert/my-site/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -22,7 +22,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule, ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
   ],
