afdce52e735022cff3542dc4bafc4d51492592e9
Hastings <hastingsl@101grd-9-pc01.tigerworld.savstate.edu>
2017-03-27 17:00:19 -0400

    3/27/17

--- angular2Convert/my-site/src/app/header-small/header-small.component.ts
+++ angular2Convert/my-site/src/app/header-small/header-small.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+declare var $:any
 
 @Component({
   selector: 'app-header-small',
   templateUrl: './header-small.component.html',
   styleUrls: ['./header-small.component.css']
 })
-export class HeaderSmallComponent implements OnInit {
+export class HeaderSmallComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    $('#menu-button').click(function(){
+      $('#nav-list').toggleClass('hide');
+    });
   }
 
 }
