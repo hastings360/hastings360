090e945095285a5d4fbea9881de4e4166de2038d
Hastings <hastingsl@106hdg-pc02.tigerworld.savstate.edu>
2017-04-20 13:14:10 -0400

    4/20

--- angular2Convert/hello-world/src/app/user-item/user-item.component.ts
+++ angular2Convert/hello-world/src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
-  name: string; // name property
+  @Input() name: string; // Input annotation
 
   constructor() { 
-    this.name = 'Felipe';  //set the name to Felipe
   }
 
   ngOnInit() {
