8720c833b1848269e602fd43110ff5f12164c54a
Hastings <hastingsl@101grd-9-pc01.tigerworld.savstate.edu>
2017-04-26 16:46:47 -0400

    updated contact submit, needs function to do something

--- angular2Convert/my-site/src/app/contact/contact.component.ts
+++ angular2Convert/my-site/src/app/contact/contact.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
- emailMe(name: HTMLInputElement, emil: HTMLInputElement, number: HTMLInputElement, message: HTMLInputElement):boolean{
+ emailMe(name: HTMLInputElement, email: HTMLInputElement, number: HTMLInputElement, message: HTMLInputElement):boolean{
       /*email myself*/
     return false;
  }
