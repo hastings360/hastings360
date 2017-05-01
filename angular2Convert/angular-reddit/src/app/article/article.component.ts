c19517407fb5113809bfad45fd53c3c4cd28c7b4
Hastings <hastingsl@106hdg-pc02.tigerworld.savstate.edu>
2017-05-01 16:23:20 -0400

    update

--- angular2Convert/angular-reddit/src/app/article/article.component.ts
+++ angular2Convert/angular-reddit/src/app/article/article.component.ts
@@ -1,3 +1,4 @@
+import { Article } from './article.model';
 import { Component, OnInit ,HostBinding} from '@angular/core';
 
 @Component({
@@ -8,21 +9,22 @@ import { Component, OnInit ,HostBinding} from '@angular/core';
 export class ArticleComponent implements OnInit {
 
   @HostBinding('attr.class') cssClass = 'row';
-                              votes: number;
-                              title: string;
-                              link: string;
+  article: Article;
+
   constructor() { 
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    this.article = new Article(
+      'Angular 2',
+      'http://angular.io',
+      10
+    );
   }
 
   voteUp(): boolean{
-    this.votes +=1;
+    this.article.voteUp();
     return false;
   }
   voteDown(): boolean{
-    this.votes -=1;
+    this.article.voteDown();
     return false;
   }
   
