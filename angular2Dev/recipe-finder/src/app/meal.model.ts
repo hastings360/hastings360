529f0d7e21b35cdf2f65c3e53abe562a7d23000b
Hastings <hastingsl@101grd-9-pc01.tigerworld.savstate.edu>
2017-06-06 17:09:09 -0400

    added class

---
+++ angular2Dev/recipe-finder/src/app/meal.model.ts
@@ -0,0 +1,17 @@
+import { DateFormatter } from '@angular/common/src/pipes/intl';
+/**
+ * meal object
+ */
+export class Meal{
+    constructor(
+        public imageUrl: string,
+        public name: string,
+        public description: string,
+        public itemNumber: number,
+        public contributor: string,
+        public ingredients: string[],
+        public contributionDate: DateFormatter,
+        public estimatedCalories: number,
+        public directions: string
+    ){}
+}
