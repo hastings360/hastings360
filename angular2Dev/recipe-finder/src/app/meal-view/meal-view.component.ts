e5fd58790d18136799cbd8ce0bb957c6d4f378bb
Hastings <hastingsl@106hdg-pc02.tigerworld.savstate.edu>
2017-06-08 16:56:30 -0400

    update

---
+++ angular2Dev/recipe-finder/src/app/meal-view/meal-view.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { Meal } from '../meal.model';
+
+@Component({
+  selector: 'app-meal-view',
+  templateUrl: './meal-view.component.html',
+  styleUrls: ['./meal-view.component.css']
+})
+export class MealViewComponent {
+  bakedTurkeyWrap: Meal;
+
+  constructor(){
+    this.bakedTurkeyWrap = new Meal(
+      'baked_turkey_wrap.jpg',
+      'Baked Turkey Wrap',
+      'Baked turkey with asparagus and condiments',
+      1,
+      'Larry Hastings',
+      ['turkey thigh',' asparagus',' mayonaise',' chow chow',' your choice of wrap', ' cajun seasoning'],
+      '06/06/2017',
+      500,
+      'Bake turkey in cast iron pan for about an hour and 15 minutes.  Remove turkey, and place pan over medium heat.  Add asparagus to left over contents to instill the flavors from the cooked turkey onto the asparagus.  This takes about 10 minutes.  While doing this, add a little mayonaise and chow chow to the wrap.  Cut off small pieces of the turkey and add them to the wrap as well.  Once asparagus is done, add it whole or cut it up for easier "mouth fiting." That\'s it, enjoy!     Bake turkey in cast iron pan for about an hour and 15 minutes.  Remove turkey, and place pan over medium heat.  Add asparagus to left over contents to instill the flavors from the cooked turkey onto the asparagus.  This takes about 10 minutes.  While doing this, add a little mayonaise and chow chow to the wrap.  Cut off small pieces of the turkey and add them to the wrap as well.  Once asparagus is done, add it whole or cut it up for easier "mouth fiting." That\'s it, enjoy!'
+    );
+  }
+}
